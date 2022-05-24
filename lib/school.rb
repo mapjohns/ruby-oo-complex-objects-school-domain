216290c85a00c9bf9565171d80e68cbb9ff7ba52
Matthew <mattj20@gmail.com>
2022-05-23 17:40:03 -0700

    commit tree 7b0dfe1df556cfd8495c1658a553229662bc7bea

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster.keys.include?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            @roster[grade] = @roster[grade].sort
+        end
+        @roster
+    end
+end
